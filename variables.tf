393d05b4b61cf37f0efe8b7aacb5e778d06b72d0
Jan Nabbefeld <jan@odise.de>
2020-11-25 15:22:11 +0100

    feat: Github actions
    chore: terraform version
    
    chore: bump version
    
    chore: AWS_REGION=eu-central-1
    
    feat: terraform-docs
    
    chore: terraform-docs automated action
    
    feat: tflint
    
    chore: terraform-docs automated action
    
    chore: indention
    
    chore: pre-commit, variables types
    
    chore: tflint
    
    chore: terraform-docs automated action
    
    chore: Ubuntu-20.04
    
    chore: GH Actions
    
    chore: variable type
    
    chore: terraform-docs automated action
    
    chore: readme

--- variables.tf
+++ variables.tf
@@ -1,37 +1,156 @@
-variable project_namespace { default = "terraform-aws-ec2-instance" }
-variable project_stage { default = "dev" }
-variable instance_name { default = "ec2-instance" }
-variable ami { default = "ami-0555c8a4c6ccc7aef" }
-variable instance_type { default = "c4.large" }
-variable disable_api_termination { default = true }
-variable root_block_device_size { default = 20 }
-variable root_block_device_delete_on_termination { default = true }
-variable root_block_device_iops { default = null }
-variable ebs_optimized { default = null }
-variable ebs_block_device { default = [] }
-variable buildin_ebs_block_device { default = [] }
-variable backup_volumes { default = true }
-variable backup_volumes_schedule { default = "cron(0 1 * * ? *)" }
-variable backup_volumes_delete_after { default = 30 }
-variable subnet_id { default = "" }
-variable private_ip { default = "" }
-variable vpc_security_group_ids { default = [] }
-variable associate_public_ip_address { default = false }
-variable key_name { default = "" }
-variable iam_instance_profile { default = "" }
-variable hosted_zone_name { default = "" }
-variable hosted_zone_id { default = "" }
-variable route53_record { default = "" }
-variable assign_eip { default = false }
-variable ebs_kms_key_arn { default = "" }
-variable user_data { default = "" }
+variable project_namespace {
+  description = ""
+  default     = "terraform-aws-ec2-instance"
+  type        = string
+}
+variable project_stage {
+  description = ""
+  default     = "dev"
+  type        = string
+}
+variable instance_name {
+  description = ""
+  default     = "ec2-instance"
+  type        = string
+}
+variable ami {
+  description = ""
+  default     = "ami-0555c8a4c6ccc7aef"
+  type        = string
+}
+variable instance_type {
+  description = ""
+  default     = "c4.large"
+  type        = string
+}
+variable disable_api_termination {
+  description = ""
+  default     = true
+  type        = bool
+}
+variable root_block_device_size {
+  description = ""
+  default     = 20
+  type        = number
+}
+variable root_block_device_delete_on_termination {
+  description = ""
+  default     = true
+  type        = bool
+}
+variable root_block_device_iops {
+  description = ""
+  default     = null
+  type        = number
+}
+variable ebs_optimized {
+  description = ""
+  default     = null
+  type        = bool
+}
+variable ebs_block_device {
+  description = ""
+  default     = []
+  type        = list
+}
+variable buildin_ebs_block_device {
+  description = ""
+  default     = []
+  type        = list
+}
+variable backup_volumes {
+  description = ""
+  default     = true
+  type        = bool
+}
+variable backup_volumes_schedule {
+  description = ""
+  default     = "cron(0 1 * * ? *)"
+  type        = string
+}
+variable backup_volumes_delete_after {
+  description = ""
+  default     = 30
+  type        = number
+}
+variable subnet_id {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable private_ip {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable vpc_security_group_ids {
+  description = ""
+  default     = []
+  type        = list
+}
+variable associate_public_ip_address {
+  description = ""
+  default     = false
+  type        = bool
+}
+variable key_name {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable iam_instance_profile {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable hosted_zone_name {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable hosted_zone_id {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable route53_record {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable assign_eip {
+  description = ""
+  default     = false
+  type        = bool
+}
+variable ebs_kms_key_arn {
+  description = ""
+  default     = ""
+  type        = string
+}
+variable user_data {
+  description = ""
+  default     = ""
+  type        = string
+}
 variable cpu_credits {
   type        = string
   default     = "standard"
   description = "T2/T3 Unlimited configuration. Can be `standard` and `unlimited`."
 }
-
-variable instance_tags { default = {} }
-variable volume_tags { default = {} }
-variable backup_tags { default = {} }
+variable instance_tags {
+  description = ""
+  default     = {}
+  type        = map
+}
+variable volume_tags {
+  description = ""
+  default     = {}
+  type        = map
+}
+variable backup_tags {
+  description = ""
+  default     = {}
+  type        = map
+}
 
